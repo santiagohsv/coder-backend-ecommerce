d963fbf926e9ed7946a5dc2cf3aa4caa8702fb10
unknown <santiago.hsv@gmail.com>
2022-08-15 20:55:33 -0300

    change middleware response text

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,7 +5,7 @@ import env from "../config/index";
 export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
 
   const token = req.headers['auth-token']  as string
-  if (!token) return res.status(401).json({ msg: 'No hay token, Unauthorized' });
+  if (!token) return res.status(401).json({ msg: 'Unauthorized' });
   try {
     const user = jwt.verify(token as string, env.JWT_KEY as string) as jwt.JwtPayload
     res.locals.user = user; 
